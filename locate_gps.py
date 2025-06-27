90c4261a743f22158bf0c72be76bd909b18fc6d6
jamiesash <jamesash@hawaii.edu>
2025-06-26 14:54:55 -1000

    filanlized the gps locator.

--- locate_gps.py
+++ locate_gps.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 # --------------------------------------------------------------------------------
+# Calculates the relitive location of the gps devices to the primary device. The 
+# gps possitions are taken from the gbin directory. Currently every 120 gps 
+# postiion is read to speed up the calculation. Ulitmatly the diffeences between
+# the gps readings and the primary gps device readings are rotated by the ships
+# heading. 
+# --------------------------------------------------------------------------------
 # Libraries. 
 
 import argparse
 import os
-import toml
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
 from pathlib import Path
+from datetime import datetime, timedelta
 from pycurrents.file.binfile_n import BinfileSet
 from pycurrents.data import navcalc
 from pycurrents.num import interp1 
@@ -21,31 +26,64 @@ from pycurrents.adcp.uhdas_cfg_api import get_cfg, find_cfg_files
 parser = argparse.ArgumentParser()
 parser.add_argument("--uhdas_dir", 
                    default = str(Path().resolve()),
-                   help="uhdas directory you want to access.",
+                   help="uhdas directory you want to access. Default is the current directory.",
                    type = str)
+parser.add_argument("--sdate", 
+                   default = False,
+                   help="Start time for subset. String format as 2025-01-01 00:00:00. Default is the entire cruise.",
+                   type = str)
+parser.add_argument("--edate", 
+                   default = False,
+                   help="End time for subset. String format as 2025-01-01 00:00:00. Default is the entire cruise.",
+                   type = str)
+parser.add_argument("--subsample", 
+                   default = 120,
+                   help="The subsample amount that is used for Binfileset. Default is every 120 samples. Input an integer.",
+                   type = int)
+parser.add_argument("--verbose", 
+                   default = False,
+                   help="Output the table to the terminal. Input either True or Flase. Default is Flase.",
+                   type = bool)
 
 args = parser.parse_args()
 path_cruise = args.uhdas_dir
+subsample = args.subsample
+verbose = args.verbose
+sdate = args.sdate
+edate = args.edate
 
 # --------------------------------------------------------------------------------
 # functions and subroutines
 
-# give a list of rbin files aoutput catinated array.
-def arrayrbins(files):
-    mat = list()
-    for i in files:
-        tmp = BinfileSet(str(i))
-        mat.append(tmp.array)
-    mat = np.vstack(mat)
-    return(mat) 
-
 # generates list of rbin files and calls arrayrbins.
-def readrbins(pth, sensor, tag):
+def list_files(path, sensor, tag):
     tag = "*" + tag + "*.rbin"
-    files = sorted(Path(pth+sensor+"/").glob(tag))
-    mat = arrayrbins(files)
-    #cols = BinfileSet(str(files[0])).columns
-    mat = np.array(mat) #, dtype=cols)
+    files = sorted(Path(path+sensor+"/").glob(tag))
+    return(files)
+    
+def read_nav(files, cruise_year, step, sdate = True, edate = True):
+    # read them matfiles. 
+    tmp = BinfileSet(files, step = step)
+    # get into numpy's world. 
+    mat = np.array(tmp.array)
+    days = mat[:,2] 
+
+    # convert to an unambgous datetime format. 
+    cruise_year = datetime.strptime(cruise_year, '%Y').year
+    since_start = datetime(cruise_year, 1, 1)      # This is the "days since" part
+    tmp = list()
+    for i in range(0, days.shape[0]):
+        # from file_id.variables: origin 2024-01-01 00:00:00
+        delta = timedelta(days[i])     # Create a time delta object from the number of days
+        offset = since_start + delta    # Add the specified number of days to the start of the year
+        tmp.append(offset.strftime('%Y-%m-%d %H:%M:%S'))
+    time = np.array(tmp)
+
+    # cut the cruise data by the given string start and stop times. 
+    if isinstance(sdate, str):
+        time_idx = np.where((time > sdate) & (time < edate))
+        mat = mat[time_idx[0],:]
+
     return(mat)
 
 def carvect(primary, sensor, hdg):
@@ -66,7 +104,6 @@ def carvect(primary, sensor, hdg):
 # find the location on the ship
 def shiplocal(vects, hdg):
     # Rotates the distance vector by the heading. 
-    # Done via a change of basis. 
     location = []
     for i in range(1, len(hdg)):
         tht = hdg[i,1]*(np.pi/180) # to radians
@@ -79,14 +116,15 @@ def shiplocal(vects, hdg):
 # input is matrix of dimensions [x,2] where x is any number.
 # columns are the dx dy.   
 def xymean(mat):
-    ux = np.nanmean(mat[:, 0])
-    uy = np.nanmean(mat[:, 1])
+    ux = np.nanmedian(mat[:, 0])
+    uy = np.nanmedian(mat[:, 1])
     cent = np.array([ux, uy])
     return(cent)
 
 # --------------------------------------------------------------------------------
-# loading data
+# upload the data
 
+# loading data
 files = find_cfg_files(path_cruise + "/raw/config/")
 if files['sensor'] is None: 
     sys.exit("Error: No /raw/config directory found. Are you in a UHDAS cruise directory?")
@@ -110,21 +148,28 @@ badies = list()
 sensors = list()
 for i in gps:
     if any(File.endswith(".gps.rbin") for File in os.listdir(path_cruise + '/rbin/' + i)):
-        sensors.append(readrbins(pth = path_cruise + '/rbin/', sensor = i, tag = 'gps'))
+        files = list_files(path = path_cruise + '/rbin/', sensor = i, tag = 'gps')
+        tmp = read_nav(files, step = subsample, cruise_year = '2024', sdate = sdate, edate = edate)
+        sensors.append(tmp)
     else:
         badies.append(i)
-        print("No gps files in the " + i + " rbins directory.")
+        if verbose == True: print("No gps files in the " + i + " rbins directory.")
 
 # remove gps devises without gps.rbin files. 
 for i in badies:
     gps.remove(i) 
 
-# reading in the gps coordinates from the rbin. 
-prime = readrbins(pth = path_cruise + '/rbin/', sensor = pos_inst, tag = 'gps')
-head  = readrbins(pth = path_cruise + '/rbin/', sensor = hdg_inst, tag = 'hdg')
+# reading in the gps coordinates for the position device. 
+files = list_files(path = path_cruise + '/rbin/', sensor = pos_inst, tag = 'gps')
+prime = read_nav(files, step = subsample, cruise_year = '2024', sdate = sdate, edate = edate)
+
+# reading in the gps coordinates for heading device. 
+files = list_files(path = path_cruise + '/rbin/', sensor = hdg_inst, tag = 'hdg')
+head  = read_nav(files, step = subsample, cruise_year='2024', sdate = sdate, edate = edate)
 
 # --------------------------------------------------------------------------------
 # calculations
+
 gps_locations = list()
 for device in sensors:
     tmp = carvect(primary = prime, sensor = device, hdg = head)
@@ -133,58 +178,42 @@ for device in sensors:
 gps_locations = np.array(gps_locations)
 
 # --------------------------------------------------------------------------------
-# print a table of the gps lacations.
-print("    x |      y |           gps name")
-print("-----------------------------------")
-print(f"   hdg |    hdg | {hdg_inst:>18s}") 
-for ((x, y), name) in zip(gps_locations, gps):
-    print(f"{x:>6.2f} | {y:6.2f} | {name:>18s}")
-for sonar in adcps:
-    x = proc_file['xducer_dx'][sonar]
-    y = proc_file['xducer_dy'][sonar]
-    print(f"{x:>6.2f} | {y:6.2f} | {sonar:>18s}")
-print("-----------------------------------")
+# write the file to the current cruise directory.
 
-# --------------------------------------------------------------------------------
-# print a table of the gps lacations relative to each sonar.
+cruise_name = path_cruise.split("/")[-1]
+output_filename = cruise_name + "_gps_locations.txt"
 
-for sonar in adcps:
-    x = proc_file['xducer_dx'][sonar]
-    y = proc_file['xducer_dy'][sonar]
-    adcp_location = np.array([x,y])
-    adjusted = (adcp_location - gps_locations)
-    print("-----------------------------------")
-    print("GPS locations relative to the " + sonar)
-    print("-----------------------------------")
-    print("    x |      y |           gps name")
-    print("-----------------------------------")
-    print(f"   hdg |    hdg | {hdg_inst:>18s}") 
-    for ((x, y), name) in zip(adjusted, gps):
-        print(f"{x:>6.2f} | {y:6.2f} | {name:>18s}")
-    print("-----------------------------------")
+with open(output_filename, 'w') as f:
+    # Print a table of the gps locations to the specified file 'f'
+    print("GPS locations relative to the primary gps device.", file=f)
+    print("    x |      y |           gps name", file=f)
+    print("-----------------------------------", file=f)
+    print(f"   hdg |    hdg | {hdg_inst:>18s}", file=f)
+    for ((x, y), name) in zip(gps_locations, gps):
+        print(f"{x:>6.2f} | {y:6.2f} | {name:>18s}", file=f)
+    for sonar in adcps:
+        # Ensure 'sonar' is a valid key in 'proc_file['xducer_dx']' before accessing
+        if sonar in proc_file['xducer_dx'] and sonar in proc_file['xducer_dy']:
+            x = proc_file['xducer_dx'][sonar]
+            y = proc_file['xducer_dy'][sonar]
+            print(f"{x:>6.2f} | {y:6.2f} | {sonar:>18s}", file=f)
+        else:
+            print(f"Warning: Transducer offsets for '{sonar}' not found in proc_file.", file=f)
+    print("-----------------------------------", file=f)
 
-# --------------------------------------------------------------------------------
-# plot the centerpoints.
-
-xmax = np.nanmax(gps_locations[:,0])
-ymax = np.nanmax(gps_locations[:,1])
-xmin = np.nanmin(gps_locations[:,0])
-ymin = np.nanmin(gps_locations[:,1])
-
-fig = plt.figure(figsize=(5, 5))
-plt.plot(gps_locations[:,0], gps_locations[:,1], "kx", markersize = 6, alpha = 1)
-for i in range(0, len(gps)):
-    plt.annotate(gps[i], (gps_locations[i,0]+0.5, gps_locations[i, 1]-0.5), size = 10)
-for sonar in proc_file['xducer_dx']:
-    x = proc_file['xducer_dx'][sonar]
-    y = proc_file['xducer_dy'][sonar]
-    plt.plot(x, y, "kx", markersize = 6, alpha = 1)
-    plt.annotate(sonar, (x+0.5, y-0.5), size = 10)
-plt.xlabel("Horizontal distance [m]", size = 12)
-plt.ylabel("Vertical distance [m]", size = 12)
-plt.title("GPS distances from origin", size = 12)
-plt.xlim(-35, 35)
-plt.ylim(ymin - 5, 35)
-plt.gca().set_aspect(1)
-plt.show()
-
+# print output to the terminal. 
+if verbose == True:
+    # # print a table of the gps lacations.
+    # print("GPS locations relative to the primary gps device.")
+    # print("    x |      y |           gps name")
+    # print("-----------------------------------")
+    # print(f"   hdg |    hdg | {hdg_inst:>18s}") 
+    # for ((x, y), name) in zip(gps_locations, gps):
+    #     print(f"{x:>6.2f} | {y:6.2f} | {name:>18s}")
+    # for sonar in adcps:
+    #     x = proc_file['xducer_dx'][sonar]
+    #     y = proc_file['xducer_dy'][sonar]
+    #     print(f"{x:>6.2f} | {y:6.2f} | {sonar:>18s}")
+    # print("-----------------------------------")
+    # Confirm to the user that the file has been saved (this print goes to console)
+    print(f"Table successfully saved as {output_filename}.")
--- locate_gps_old.py
+++ locate_gps_old.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
 # --------------------------------------------------------------------------------
-# Calculates the relitive location of the gps devices to the primary device. The 
-# gps possitions are taken from the gbin directory. Currently every 120 gps 
-# postiion is read to speed up the calculation. Ulitmatly the diffeences between
-# the gps readings and the primary gps device readings are rotated by the ships
-# heading. 
-# --------------------------------------------------------------------------------
 # Libraries. 
 
 import argparse
 import os
+import toml
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 from pathlib import Path
-from datetime import datetime, timedelta
 from pycurrents.file.binfile_n import BinfileSet
 from pycurrents.data import navcalc
 from pycurrents.num import interp1 
@@ -28,51 +23,29 @@ parser.add_argument("--uhdas_dir",
                    default = str(Path().resolve()),
                    help="uhdas directory you want to access.",
                    type = str)
-parser.add_argument("--sdate", 
-                   default = False,
-                   help="Start time for subset. String format as '%Y-%m-%d %H:%M:%S'.",
-                   type = str)
-parser.add_argument("--edate", 
-                   default = False,
-                   help="End time for subset. String format as '%Y-%m-%d %H:%M:%S'.",
-                   type = str)
+
 args = parser.parse_args()
 path_cruise = args.uhdas_dir
-sdate = args.sdate
-edate = args.edate
 
 # --------------------------------------------------------------------------------
 # functions and subroutines
 
+# give a list of rbin files aoutput catinated array.
+def arrayrbins(files):
+    mat = list()
+    for i in files:
+        tmp = BinfileSet(str(i))
+        mat.append(tmp.array)
+    mat = np.vstack(mat)
+    return(mat) 
+
 # generates list of rbin files and calls arrayrbins.
-def list_files(path, sensor, tag):
+def readrbins(pth, sensor, tag):
     tag = "*" + tag + "*.rbin"
-    files = sorted(Path(path+sensor+"/").glob(tag))
-    return(files)
-    
-def read_nav(files, cruise_year, sdate = True, edate = True):
-    # read them matfiles. 
-    tmp = BinfileSet(files, step = 120)
-    # get into numpy's world. 
-    mat = np.array(tmp.array)
-    days = mat[:,2] 
-
-    # convert to an unambgous datetime format. 
-    cruise_year = datetime.strptime(cruise_year, '%Y').year
-    since_start = datetime(cruise_year, 1, 1)      # This is the "days since" part
-    tmp = list()
-    for i in range(0, days.shape[0]):
-        # from file_id.variables: origin 2024-01-01 00:00:00
-        delta = timedelta(days[i])     # Create a time delta object from the number of days
-        offset = since_start + delta    # Add the specified number of days to the start of the year
-        tmp.append(offset.strftime('%Y-%m-%d %H:%M:%S'))
-    time = np.array(tmp)
-
-    # cut the cruise data by the given string start and stop times. 
-    if isinstance(sdate, str):
-        time_idx = np.where((time > sdate) & (time < edate))
-        mat = mat[time_idx[0],:]
-
+    files = sorted(Path(pth+sensor+"/").glob(tag))
+    mat = arrayrbins(files)
+    #cols = BinfileSet(str(files[0])).columns
+    mat = np.array(mat) #, dtype=cols)
     return(mat)
 
 def carvect(primary, sensor, hdg):
@@ -93,6 +66,7 @@ def carvect(primary, sensor, hdg):
 # find the location on the ship
 def shiplocal(vects, hdg):
     # Rotates the distance vector by the heading. 
+    # Done via a change of basis. 
     location = []
     for i in range(1, len(hdg)):
         tht = hdg[i,1]*(np.pi/180) # to radians
@@ -105,15 +79,14 @@ def shiplocal(vects, hdg):
 # input is matrix of dimensions [x,2] where x is any number.
 # columns are the dx dy.   
 def xymean(mat):
-    ux = np.nanmedian(mat[:, 0])
-    uy = np.nanmedian(mat[:, 1])
+    ux = np.nanmean(mat[:, 0])
+    uy = np.nanmean(mat[:, 1])
     cent = np.array([ux, uy])
     return(cent)
 
 # --------------------------------------------------------------------------------
-# upload the data
-
 # loading data
+
 files = find_cfg_files(path_cruise + "/raw/config/")
 if files['sensor'] is None: 
     sys.exit("Error: No /raw/config directory found. Are you in a UHDAS cruise directory?")
@@ -137,9 +110,7 @@ badies = list()
 sensors = list()
 for i in gps:
     if any(File.endswith(".gps.rbin") for File in os.listdir(path_cruise + '/rbin/' + i)):
-        files = list_files(path = path_cruise + '/rbin/', sensor = i, tag = 'gps')
-        tmp = read_nav(files, cruise_year = '2024', sdate = sdate, edate = edate)
-        sensors.append(tmp)
+        sensors.append(readrbins(pth = path_cruise + '/rbin/', sensor = i, tag = 'gps'))
     else:
         badies.append(i)
         print("No gps files in the " + i + " rbins directory.")
@@ -148,17 +119,12 @@ for i in gps:
 for i in badies:
     gps.remove(i) 
 
-# reading in the gps coordinates for the position device. 
-files = list_files(path = path_cruise + '/rbin/', sensor = pos_inst, tag = 'gps')
-prime = read_nav(files, cruise_year = '2024', sdate = sdate, edate = edate)
-
-# reading in the gps coordinates for heading device. 
-files = list_files(path = path_cruise + '/rbin/', sensor = hdg_inst, tag = 'hdg')
-head  = read_nav(files, cruise_year='2024', sdate = sdate, edate = edate)
+# reading in the gps coordinates from the rbin. 
+prime = readrbins(pth = path_cruise + '/rbin/', sensor = pos_inst, tag = 'gps')
+head  = readrbins(pth = path_cruise + '/rbin/', sensor = hdg_inst, tag = 'hdg')
 
 # --------------------------------------------------------------------------------
 # calculations
-
 gps_locations = list()
 for device in sensors:
     tmp = carvect(primary = prime, sensor = device, hdg = head)
@@ -167,10 +133,7 @@ for device in sensors:
 gps_locations = np.array(gps_locations)
 
 # --------------------------------------------------------------------------------
-# print output
-
 # print a table of the gps lacations.
-print("GPS locations relative to the primary gps device.")
 print("    x |      y |           gps name")
 print("-----------------------------------")
 print(f"   hdg |    hdg | {hdg_inst:>18s}") 
@@ -181,3 +144,47 @@ for sonar in adcps:
     y = proc_file['xducer_dy'][sonar]
     print(f"{x:>6.2f} | {y:6.2f} | {sonar:>18s}")
 print("-----------------------------------")
+
+# --------------------------------------------------------------------------------
+# print a table of the gps lacations relative to each sonar.
+
+for sonar in adcps:
+    x = proc_file['xducer_dx'][sonar]
+    y = proc_file['xducer_dy'][sonar]
+    adcp_location = np.array([x,y])
+    adjusted = (adcp_location - gps_locations)
+    print("-----------------------------------")
+    print("GPS locations relative to the " + sonar)
+    print("-----------------------------------")
+    print("    x |      y |           gps name")
+    print("-----------------------------------")
+    print(f"   hdg |    hdg | {hdg_inst:>18s}") 
+    for ((x, y), name) in zip(adjusted, gps):
+        print(f"{x:>6.2f} | {y:6.2f} | {name:>18s}")
+    print("-----------------------------------")
+
+# --------------------------------------------------------------------------------
+# plot the centerpoints.
+
+xmax = np.nanmax(gps_locations[:,0])
+ymax = np.nanmax(gps_locations[:,1])
+xmin = np.nanmin(gps_locations[:,0])
+ymin = np.nanmin(gps_locations[:,1])
+
+fig = plt.figure(figsize=(5, 5))
+plt.plot(gps_locations[:,0], gps_locations[:,1], "kx", markersize = 6, alpha = 1)
+for i in range(0, len(gps)):
+    plt.annotate(gps[i], (gps_locations[i,0]+0.5, gps_locations[i, 1]-0.5), size = 10)
+for sonar in proc_file['xducer_dx']:
+    x = proc_file['xducer_dx'][sonar]
+    y = proc_file['xducer_dy'][sonar]
+    plt.plot(x, y, "kx", markersize = 6, alpha = 1)
+    plt.annotate(sonar, (x+0.5, y-0.5), size = 10)
+plt.xlabel("Horizontal distance [m]", size = 12)
+plt.ylabel("Vertical distance [m]", size = 12)
+plt.title("GPS distances from origin", size = 12)
+plt.xlim(-35, 35)
+plt.ylim(ymin - 5, 35)
+plt.gca().set_aspect(1)
+plt.show()
+
